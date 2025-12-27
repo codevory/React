16e9c410827a1a62c40222dacb5ea3a7c3c21ebb
codevory <shahijahan.codes@gmail.com>
2025-12-27 14:36:06 +0530

    improved body ui & fixed footer collapsing bugs

--- Blog-project/src/App.jsx
+++ Blog-project/src/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from 'react'
 import './index.css'
 import Home from './pages/Home'
-import CreatePost from './components/CreatePost.jsx'
+import CreatePost from '../src/components/PostComponents/CreatePost.jsx'
 import { Outlet, Route, Routes } from "react-router-dom"
 import PagenotFound from './pages/PagenotFound'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Singleblog from './components/SingleBlogView.jsx'
-import EditPost from './components/Editpost.jsx'
+import Navbar from '../src/components/BodyComponents/Navbar.jsx'
+import Singleblog from '../src/components/PostComponents/SingleBlogView.jsx'
+import EditPost from '../src/components/PostComponents/Editpost.jsx'
+import Footer from './components/BodyComponents/Footer.jsx'
 
 function App() {
  
-  return <div className='w-full min-h-screen overflow-hidden bg-white text-black dark:bg-black dark:text-white'>
+  return <div className='w-full min-h-screen overflow-hidden bg-white text-zinc-800 dark:bg-zinc-900 dark:text-white'>
        <Navbar />
        <Routes>
         <Route path='/' element={<Home />} />
--- Blog-project/src/pages/Home.jsx
+++ Blog-project/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom"
 import {useMemo , useState} from 'react'
-import Footer from "../components/Footer"
-import Navbar from "../components/Navbar"
-import Posts from "../components/Posts"
-import Sortpost from '../components/Sortpost'
-import Search from '../components/Search'
+import Footer from "../components/BodyComponents/Footer"
+import Navbar from "../components/BodyComponents/Navbar"
+import Posts from "../components/PostComponents/Posts"
+import Sortpost from '../components/FilterComponents/Sortpost'
+import Search from '../components/FilterComponents/Search'
+import Categorysorting from "../components/FilterComponents/Categorysorting"
 
 
 const Home = () => {
@@ -12,27 +13,64 @@ const Home = () => {
   let dumy ;
   const [searchQuery, setsearchQuery] = useState("")
   const [sortedOrder,setSortedOrder] = useState("newest")
+  const [categoryFilter,setCategoryFilter] = useState("")
   let posts = JSON.parse(localStorage.getItem("postData")) || []
-  // if(posts.length <= 0){
-  //   localStorage.setItem("postData",JSON.stringify(dumy))
-  // }
-  // else{
-  //   posts = posts;
-  // }
 
 
-  const finalPosts = useMemo(() => {
-    let filteredPost = posts.filter(post => post.title.toLowerCase().includes(searchQuery.toLowerCase()))
+  const sortedPost = useMemo(() => {
+  let filteredPost = posts.filter(post => post.title.toLowerCase().includes(searchQuery.toLowerCase()))
 
     filteredPost.sort((a,b)=>{
       if(sortedOrder === "newest") return b.createdAt - a.createdAt;
       return a.createdAt - b.createdAt;
     })
+   
     return filteredPost
   },[searchQuery,sortedOrder,posts])
 
-  return (
-    <div className='flex flex-col px-6 py-2 min-h-screen '>
+  let categSorted;
+let FinalPosts = useMemo(() => {
+categSorted = sortedPost.filter(post => post.category.toLowerCase().includes(categoryFilter.toLowerCase()))
+return categSorted
+},[categoryFilter,posts])
+
+//check device width.
+if(window.innerWidth < 350){
+  alert("Kindly turn on the desktop mode for better view")
+}
+
+//if post category doesn't matches the category filter
+  if(!categSorted){
+    return (
+       <div className='flex flex-col px-3 md:px-6 py-2 min-h-screen '>
+        <div className=' flex justify-between items-center py-1 w-full'>
+
+<span className='block ml-2 w-[40%]'>
+
+             <Sortpost onSort={setSortedOrder} />
+</span>
+
+<span className='block ml-2 w-[25%]'>
+
+      <Categorysorting onSort={setCategoryFilter} />
+</span>
+
+<span className='block mr-1 w-[40%] '>
+
+             <Search onSearch={setsearchQuery} />
+</span>
+
+
+        </div>
+        <div className='flex justify-center items-center font-semibold gap-3.5 mt-3'>
+      <p>{`No match found for "${categoryFilter}"`}</p>
+        </div>
+    </div>
+    )
+  }
+  //If post not matched on search will return paragraph
+     if(!sortedPost.length) return(
+      <div className='flex flex-col px-3 md:px-6 py-2 min-h-screen '>
         <div className=' flex justify-between items-center py-1 w-full'>
 
 <span className='block ml-2 w-[40%]'>
@@ -43,11 +81,41 @@ const Home = () => {
 
              <Search onSearch={setsearchQuery} />
 </span>
+
+        </div>
+        <div className='flex justify-center items-center font-semibold gap-3.5 mt-3'>
+      <p>{`No match found for "${searchQuery}"`}</p>
+        </div>
+    </div>
+     )
+
+//Final return 
+  return (
+    <div className='flex flex-col px-3 md:px-6 py-2 min-h-screen '>
+        <div className=' flex justify-between items-center py-1 w-full'>
+
+<span className='block ml-2 w-[25%]'>
+
+             <Sortpost onSort={setSortedOrder} />
+</span>
+
+<span className='block ml-2 w-[25%]'>
+
+      <Categorysorting onSort={setCategoryFilter} />
+</span>
+
+<span className='block mr-1 w-[40%] '>
+
+             <Search onSearch={setsearchQuery} />
+</span>
           
 
         </div>
         <div className='flex flex-col gap-3.5'>
-      <Posts posts={finalPosts} />
+      <Posts posts={FinalPosts} />
+        </div>
+        <div>
+          <Footer />
         </div>
     </div>
   )
