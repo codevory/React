23d63576199f4be7d68b537413dcb40fa105ea22
codevory <shahijahan.codes@gmail.com>
2025-12-31 22:30:17 +0530

    added proper elements for seo

--- Blog-project/src/components/PostComponents/Posts.jsx
+++ Blog-project/src/components/PostComponents/Posts.jsx
@@ -16,9 +16,9 @@ if(posts.length > 0){
         return (
             <div key={post.id} >
      <Link to={`/article/${post.slug}`}>
-       <div className=' flex flex-col gap-1 w-full min-h-40 bg-transparent border-2 rounded-[9px] px-2 py-1 shadow-md transition '>
+       <div className=' flex flex-col gap-1 h-70 min-h-40 bg-transparent border-2 rounded-[9px] px-2 py-1 shadow-md transition '>
           <div className='flex flex-col gap-1'>
-        <span className='text-[18px] font-semibold md:text-[19px] '> {post.title}</span>
+        <h1 className='text-[18px] font-semibold md:text-[19px] '> {post.title}</h1>
         <div className='flex items-center gap-1'>
          <span className='text-xs font-semibold md:text-[16px] rounded-[5px] px-2 py-1 text-blue-700 bg-blue-100'>{post.category}</span>
            <div className='flex justify-start items-center gap-2 font-mono text-[14px] md:text-[14px]'>
@@ -28,9 +28,9 @@ if(posts.length > 0){
         </div>
           </div>
           <div className=''>
-           <span className='text-[15px] font-sans md:text-[17px] max-w-3xl'><p>{getPreview(post.content,30)} {post.content.split(/\s+/).length > 30 && (
+           <p className='text-[15px] font-sans md:text-[17px] max-w-3xl'><p>{getPreview(post.content,30)} {post.content.split(/\s+/).length > 30 && (
             <span className='text-blue-800 font-bold body dark:text-blue-400'>Show more</span> )}  
-             </p></span>
+             </p></p>
           </div>
         </div>
         </Link>
