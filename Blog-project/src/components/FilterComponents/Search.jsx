17b0f045f31aea69d84db4786e12e09437a322cf
codevory <shahijahan.codes@gmail.com>
2025-12-31 15:31:43 +0530

    replaced img search png with lucid icon

--- Blog-project/src/components/FilterComponents/Search.jsx
+++ Blog-project/src/components/FilterComponents/Search.jsx
@@ -1,14 +1,16 @@
 import React,{useState} from 'react'
 import searchicon from '/searchicon.png'
+import { Search } from 'lucide-react';
 
-const Search = ({onSearch}) => {
+
+const SearchButton = ({onSearch}) => {
   return (
     <div className='w-full h-12 py-1 flex lg:justify-center items-center gap-2 md:gap-0 rounded-[7px] relative border-2 '>
-      <img className='absolute left-1 top-2.5 md:top-1.5 size-6 md:size-8 rounded' src={searchicon} style={{color:'red'}} typeof='png/img' alt='search-icon' />
+      <span className='absolute left-1 top-2.5 md:top-1.5 size-6 md:size-8 rounded'>{<Search size={28} strokeWidth={1.6} absoluteStrokeWidth />} </span>
       <input onChange={(e) => onSearch(e.target.value)} type='text' name='search' placeholder='Type to search..'
        className='text-[14px] md:text-xl absolute right-1 md:left-11 outline-0 top-0 w-[80%] h-full px-1 lg:px-0 rounded '/>
     </div>
   )
 }
 
-export default Search
+export default SearchButton
