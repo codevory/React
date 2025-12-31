d3a89f19cc7a268435d58502662e8cbde6a4f45e
codevory <shahijahan.codes@gmail.com>
2025-12-31 22:31:14 +0530

    rendered sidebar,leftmenu & improved flexbox

--- Blog-project/src/pages/Home.jsx
+++ Blog-project/src/pages/Home.jsx
@@ -2,9 +2,11 @@ import {useMemo , useState, useEffect} from 'react'
 import Footer from "../components/BodyComponents/Footer"
 import Posts from "../components/PostComponents/Posts"
 import Sortpost from '../components/FilterComponents/Sortpost'
-import Search from '../components/FilterComponents/Search'
 import Categorysorting from "../components/FilterComponents/Categorysorting"
 import SearchButton from '../components/FilterComponents/Search'
+import Sidebar from '../components/BodyComponents/Sidebar'
+import LeftMenu from '../components/BodyComponents/LeftMenu'
+import Logo from '/logoVistle.png'
 
 const Home = () => {
 
@@ -310,29 +312,43 @@ if(window.innerWidth < 350){
 
 //Final return 
   return (
-    <div className='flex flex-col px-3 md:px-6 py-2 min-h-screen '>
-        <div className=' flex justify-between items-center py-1 w-full'>
+  <div className='w-full flex flex-col px-3 md:px-6 py-2 min-h-screen '>
+    {/* filter buttons */}
+    <div className='flex flex-col md:flex-row w-full gap-4'> 
+        <div className='lg:w-1/4 flex-col  lg:px-2 lg:rounded hidden lg:flex'>
+        <img width={60} height={60} src={Logo} alt='logo' className='rounded-full ml-4'></img>
+        <LeftMenu />
+        </div>
 
-<span className='block ml-2 w-[25%]'>
+        <div className='flex w-full md:w-[70%] lg:w-1/2 flex-col  md:justify-between  px-2 rounded'>
 
+    <div className=' flex justify-between items-center py-1 '>
+     <span className='block ml-2 w-[25%]'>
              <Sortpost onSort={setSortedOrder} />
-</span>
+     </span>
 
-<span className='block ml-2 w-[25%]'>
-
+      <span className='block ml-2 w-[25%]'>
       <Categorysorting onSort={setCategoryFilter} />
-</span>
+     </span>
 
-<span className='block mr-1 w-[40%] '>
-
+     <span className='block mr-1 w-[40%] '>
              <SearchButton onSearch={setsearchQuery} />
-</span>
+     </span>
           
+    </div>
 
-        </div>
-        <div className='flex flex-col gap-3.5'>
-      <Posts posts={FinalPosts} />
-        </div>
+{/* posts & sidebar from here */}
+            <div className='flex justify-center flex-col gap-3.5 '>
+                  <Posts posts={FinalPosts} />
+            </div>
+            
+    </div>
+            <div className='w-full md:w-[33%]  px-2 rounded h-screen'>
+            <h1 className='text-4xl font-serif font-extrabold text-center'>Sidebar</h1>
+                  <Sidebar posts={FinalPosts} />
+            </div>
+
+</div>
         <div>
           <Footer />
         </div>
