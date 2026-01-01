52c33050ff6e21bc44a8c047b2beaf8284d177d4
codevory <shahijahan.codes@gmail.com>
2026-01-01 19:46:41 +0530

    Improved component ui

--- Blog-project/src/components/FilterComponents/Sortpost.jsx
+++ Blog-project/src/components/FilterComponents/Sortpost.jsx
@@ -1,8 +1,12 @@
-const Sortpost = ({ onSort }) => {
+import { useTheme } from "../Theme/ThemeContext"
 
+const Sortpost = ({ onSort }) => {
+const {isDark} = useTheme()
   return (
        <div className=''>
-      <select id="sort-options" className='md:max-w-2/3 w-full h-10 px-2 py-1 rounded border-2  ' defaultValue="newest" onChange={(e) => onSort(e.target.value)
+      <select id="sort-options" className= {`h-10 md:px-2 py-1 outline-0  rounded-[20px] px-1 md:rounded-3xl font-semibold border-2 overflow-hidden text-[13px] sm:text-[15px] 
+     ${isDark ? 'bg-white text-black hover:bg-zinc-300' : 'bg-zinc-800 text-white hover:bg-zinc-900'}`}
+      defaultValue="newest" onChange={(e) => onSort(e.target.value)
       }>
         <option value="newest">Newest</option>
         <option value="oldest">Oldest</option>
