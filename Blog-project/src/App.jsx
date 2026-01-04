36b2a559581115e4fe6936bec315687282033b8f
codevory <shahijahan.codes@gmail.com>
2026-01-04 16:12:43 +0530

    added trending page route

--- Blog-project/src/App.jsx
+++ Blog-project/src/App.jsx
@@ -7,6 +7,7 @@ import PagenotFound from './pages/PagenotFound'
 import Navbar from '../src/components/BodyComponents/Navbar.jsx'
 import Singleblog from '../src/components/PostComponents/SingleBlogView.jsx'
 import EditPost from '../src/components/PostComponents/Editpost.jsx'
+import Trending from './components/BodyComponents/Trending.jsx'
 
 function App() {
 
@@ -18,6 +19,7 @@ return <div className='w-full min-h-screen overflow-hidden '>
         <Route path='/article/:slug' element={<Singleblog />} />
         <Route path='/post' element={<CreatePost />} />
         <Route path='*' element={<PagenotFound />} />
+        <Route path='/trending' element={<Trending />} />
       </Routes>
       <Outlet />
     </div>
