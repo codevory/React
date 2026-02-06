852d5ca16c8f29a249343c1c29b3e6ece42c90e5
codevory <shahijahan.codes@gmail.com>
2026-02-06 15:06:54 +0530

    refactored code formatting

--- EcommerceApp/src/components/item/ItemCard.jsx
+++ EcommerceApp/src/components/item/ItemCard.jsx
@@ -1,8 +1,4 @@
-import React from 'react'
 import { Star } from 'lucide-react';
-import { onSearch } from '../../api/items';
-import { useDispatch, useSelector } from 'react-redux';
-import { setLoading } from '../../store/searchSlice';
 import { Link } from 'react-router-dom';
 const ItemCard = ({item}) => {
 
@@ -15,12 +11,13 @@ const slugify = (title) => title
 let res = slugify(item.title)
       return (
         <Link to={`/${slugify(item.title)}`}>
-      <div className='w-50 h-45  flex flex-col gap-1 rounded-xl hover:scale-95 bg-pink-400 '>
-        <div className='flex justify-center items-center'>
-        <img className='w-2/3 h-full object-cover object-center' src={item.thumbnail} alt={`${item.title} image`}></img>
-        </div>
-        <div className='flex gap-2 bg-yellow-300 w-full h-4 '>
-          <h2 className='text-[12px] w-full h-2'>{item.title}</h2>
+          <div className='w-50 h-45  flex flex-col gap-1 rounded-xl hover:scale-95 bg-pink-400 '>
+            <div className='flex justify-center items-center'>
+              <img className='w-2/3 h-full object-cover object-center' src={item.thumbnail} alt={`${item.title} image`}></img>
+          </div>
+          
+        <div className='flex gap-2 bg-yellow-300 w-full'>
+          <h2 className='text-[12px] w-full overflow-hidden h-5'>{item.title}</h2>
           <span className='flex gap-1 items-center text-[14px]'>{item.rating}
           <span>{<Star size={13} stroke='black' fill='black' />}</span>
           </span>
