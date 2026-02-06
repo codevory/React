1077b06ce67a5e72093a1417a16145818e5c1af0
codevory <shahijahan.codes@gmail.com>
2026-02-06 17:27:36 +0530

    added search icon with search search bar

--- EcommerceApp/src/components/Navbar.jsx
+++ EcommerceApp/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import {Link} from 'react-router'
 import { useDispatch, useSelector } from 'react-redux'
 import { setQuery} from '../store/searchSlice'
 import Cart from './Cart'
+import { Search } from 'lucide-react'
 const Navbar = () => {
 
     const dispatch = useDispatch()
@@ -17,7 +18,15 @@ const Navbar = () => {
             setText('')
             dispatch(setQuery(text))
         }}>
-      <input value={text} onChange={(e) => setText(e.target.value)} type='search' className='w-80 h-8 rounded outline-0 border '></input>
+          <span className='w-85 flex gap-1  items-center border rounded p-0.5'>
+      <input value={text} onChange={(e) => setText(e.target.value)} type='search' className='w-80 h-8 rounded outline-0 border-0 ' ></input>
+          <button onClick={(e) => {
+            e.preventDefault()
+            setText('')
+            dispatch(setQuery(text))
+        }} className='w-7 m-0 p-0 h-auto cursor-pointer'><Search /> </button>
+          </span>
+       
         </form>
         <div className='flex gap-2 items-center'>
        <Link to='/'>Home</Link>
