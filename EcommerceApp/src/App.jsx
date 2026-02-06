39d604020556029b0e0f7f8cf47f3b7db17474d8
codevory <shahijahan.codes@gmail.com>
2026-02-06 15:22:21 +0530

    added toastContainer in app

--- EcommerceApp/src/App.jsx
+++ EcommerceApp/src/App.jsx
@@ -1,24 +1,24 @@
-import { useState } from 'react'
 import {Routes,Route} from 'react-router'
 import './index.css'
-import ItemGrid from './components/item/ItemGrid'
 import Home from './pages/Home'
 import Shop from './pages/Shop'
 import Checkout from './pages/Checkout'
 import ProductCard from './components/item/ProductCard'
 import Navbar from './components/Navbar'
+import { ToastContainer } from 'react-toastify';
 
 function App() {
 
   return (
     <>
-    <Navbar />
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/shop' element={<Shop />} />
-      <Route path='/checkout' element={<Checkout />} />
-      <Route path='/:slug' element={<ProductCard />} />
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/checkout' element={<Checkout />} />
+        <Route path='/:slug' element={<ProductCard />} />
+      </Routes>
+      <ToastContainer />
     </>
   )
 }
