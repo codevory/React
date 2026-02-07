bcfc2d7415fb5076dc0df74961be27c2213f4d0e
codevory <shahijahan.codes@gmail.com>
2026-02-07 16:32:10 +0530

    improved code to maintain the cart persistance

--- EcommerceApp/src/store/cartPersistMiddleware.js
+++ EcommerceApp/src/store/cartPersistMiddleware.js
@@ -1,11 +1,22 @@
+import { addItem, clearCart, removeItem } from "./cartSlice";
+
 export const cartPersistMiddleware = (store) => (next) => (action) => {
   const result = next(action);
 
-  const state = store.getState();
-
-  if (state?.cart?.items) {
-    localStorage.setItem("cart", JSON.stringify(state.cart.items));
+  if (
+    addItem.match(action) ||
+    removeItem.match(action) ||
+    clearCart.match(action)
+  ) {
+    const items = store.getState().cart.items;
+    localStorage.setItem("cart", JSON.stringify(items));
   }
+
+  // const state = store.getState();
+
+  // if (state?.cart?.items) {
+  //   localStorage.setItem("cart", JSON.stringify(state.cart.items));
+  // }
 
   return result;
 };
