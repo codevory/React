954b23311094db6e7c84b002585508f63d3c3b4e
codevory <shahijahan.codes@gmail.com>
2026-02-07 15:43:07 +0530

    added Redux AsyncThunk to handle api responses better & removed ui state functions like toasters from redux reducers

--- EcommerceApp/src/store/cartSlice.js
+++ EcommerceApp/src/store/cartSlice.js
@@ -1,23 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { Bounce, Slide, toast } from "react-toastify";
-
+const savedCart = JSON.parse(localStorage.getItem("cart"));
 const initialState = {
-  items: [],
+  items: savedCart || [],
 };
 
 const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    setItems: (state, action) => {
+    addItem: (state, action) => {
       const existingItem = state.items.find(
         (item) => item.id === action.payload.id,
       );
-      if (!existingItem) {
-        state.items.push({ ...action.payload, qty: 1 });
-      } else {
+      if (existingItem) {
         existingItem.qty += 1;
-        console.log("Incremented qty for ID:", existingItem.id);
+      } else {
+        state.items.push({ ...action.payload, qty: 1 });
       }
     },
     removeItem: (state, action) => {
@@ -26,54 +24,8 @@ const cartSlice = createSlice({
     clearCart: (state) => {
       state.items = [];
     },
-    clearedCart: function () {
-      toast.success("Order placed successfully!", {
-        position: "top-right",
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: false,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "colored",
-        transition: Slide,
-      });
-    },
-    addedToCart: function () {
-      toast.success("added to Cart!", {
-        position: "bottom-right",
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: false,
-        draggable: false,
-        progress: undefined,
-        theme: "light",
-        transition: Bounce,
-      });
-    },
-    removedSuccessfully: function () {
-      toast.warn("Removed Successfully!", {
-        position: "bottom-right",
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: false,
-        pauseOnHover: false,
-        draggable: false,
-        progress: undefined,
-        theme: "colored",
-        transition: Bounce,
-      });
-    },
   },
 });
 
 export default cartSlice.reducer;
-export const {
-  setItems,
-  removeItem,
-  clearCart,
-  addedToCart,
-  removedSuccessfully,
-  clearedCart,
-} = cartSlice.actions;
+export const { addItem, removeItem, clearCart } = cartSlice.actions;
--- EcommerceApp/src/store/searchSlice.js
+++ EcommerceApp/src/store/searchSlice.js
@@ -1,30 +1,68 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const searchSlice = createSlice({
   name: "search",
   initialState: {
+    status: "idle",
     query: "",
-    loading: false,
     results: [],
     error: null,
   },
+
   reducers: {
     setQuery: (state, action) => {
       state.query = action.payload;
     },
-    setResults: (state, action) => {
-      state.results = action.payload;
-    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProducts.pending, (state) => {
+        ((state.status = "loading"), (state.error = null));
+      })
 
-    setLoading: (state, action) => {
-      ((state.loading = action.payload), (state.error = null));
-    },
-    setError: (state, action) => {
-      ((state.error = action.payload), (state.loading = false));
-    },
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        const products = action.payload.products;
+        if (products.length === 0) {
+          state.status = "empty";
+          state.results = [];
+        }
+        state.results = action.payload.products.map((item) => ({
+          id: item.id,
+          title: item.title,
+          image: item.images[0],
+          thumbnail: item.thumbnail,
+          price: item.price,
+          rating: item.rating,
+          category: item.category,
+          slug: item.title
+            .toLowerCase()
+            .trim()
+            .replace(/[^a-z0-9\s-]/g, "")
+            .replace(/\s+/g, "-"),
+          description:
+            item.title +
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit Soluta maiores ipsum ipsam culpa labore autem ab corrupti accusamus. Magni, vero?",
+        }));
+        state.status = "success";
+      })
+
+      .addCase(fetchProducts.rejected, (state, action) => {
+        ((state.status = "error"), (state.error = action.error.message));
+      });
   },
 });
 
-export const { setResults, setError, setLoading, setQuery } =
-  searchSlice.actions;
+export const { setQuery } = searchSlice.actions;
 export default searchSlice.reducer;
+
+export const fetchProducts = createAsyncThunk(
+  "search/fetchProducts",
+  async (query) => {
+    const result = await fetch(
+      `https://dummyjson.com/products/search?q=${query}`,
+    );
+
+    if (!result.ok) throw new Error("Failed to fetch products");
+    return await result.json();
+  },
+);
