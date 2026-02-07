4088d78c934593c0b01f951fe1e94fe881ddc55f
codevory <shahijahan.codes@gmail.com>
2026-02-07 16:30:54 +0530

    fixed missing import

--- EcommerceApp/src/components/Navbar.jsx
+++ EcommerceApp/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React,{useState,useEffect} from 'react'
 import {Link} from 'react-router'
 import { useDispatch, useSelector } from 'react-redux'
 import { setQuery} from '../store/searchSlice'
-import Cart from './Cart'
+import Cart from './CartComponent'
 import { Search } from 'lucide-react'
 const Navbar = () => {
 
