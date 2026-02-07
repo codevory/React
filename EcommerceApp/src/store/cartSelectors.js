7a55de1ed125d2a37319b1f26a6fd047dc7e1d47
codevory <shahijahan.codes@gmail.com>
2026-02-07 15:47:53 +0530

    created selectors to calculate the cartTotal & cartCount

---
+++ EcommerceApp/src/store/cartSelectors.js
@@ -0,0 +1,11 @@
+import { createSelector } from "@reduxjs/toolkit";
+
+export const selectCartItems = (state) => state.cart.items;
+
+export const selectCartTotal = createSelector([selectCartItems], (items) =>
+  items.reduce((acc, item) => acc + item.price * item.qty, 0),
+);
+
+export const selectCartCount = createSelector([selectCartItems], (items) =>
+  items.reduce((sum, item) => sum + item.qty, 0),
+);
