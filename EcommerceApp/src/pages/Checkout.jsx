b4c6cfa1291de273c1f144ab9443378205672305
codevory <shahijahan.codes@gmail.com>
2026-02-07 15:38:01 +0530

    improved code & added cartTotal from redux store inspite of calculating itself in component

--- EcommerceApp/src/pages/Checkout.jsx
+++ EcommerceApp/src/pages/Checkout.jsx
@@ -1,27 +1,18 @@
 import { useDispatch, useSelector } from 'react-redux'
-import { removeItem,removedSuccessfully,clearCart,clearedCart } from '../store/cartSlice'
+import { removeItem,clearCart } from '../store/cartSlice'
+import { selectCartTotal } from '../store/cartSelectors'
 
 const Checkout = () => {
 
   const dispatch = useDispatch()
   const {items} = useSelector((store) => store.cart)
   if(!items) return <p>Item not found</p>
-  let total = items.reduce((acc,item) => acc + item.price,0).toFixed(2);
-
-
-  //function to clear cart items in store
-  const cleared = () => {
-    if(!items) return;
-  items.map((item) => {
-  dispatch(clearCart(item))})}
+  let total = useSelector(selectCartTotal)
 
   //placeOrder function
   function placeOrder(){
-    if(!items) return;
-    if(items) dispatch(clearedCart())
-  }
+  dispatch(clearCart())}
   
-
    const divContent = items.map((item) => (
       <div key={item.id}  className='bg-slate-800 flex justify-between px-2 py-1'>
           <div className='flex gap-2 bg-pink-600 w-[80%]  h-10 rounded-xl p-2'>
@@ -32,19 +23,15 @@ const Checkout = () => {
 
         <div>
          <button onClick={() => {
-          dispatch(removeItem(item))
-          dispatch(removedSuccessfully())}} 
-          className='bg-amber-600 h-10 rounded-xl px-2 hover:bg-red-500 active:scale-95 '>Remove</button>
+          dispatch(removeItem(item))}} 
+          className='bg-amber-600 h-10 rounded-xl px-2 hover:bg-red-500 active:scale-95 cursor-pointer '>Remove</button>
         </div>
-
-    
       </div>
    ))
 
 return (
   <>
     <div className=' bg-blue-300 h-dvh px-6 py-2 flex flex-col justify-center items-center'>
-
       <div className='bg-slate-500 min-w-[40%] h-1/2 flex flex-col gap-1 px-2 py-1'>
           {divContent}
         <div className='w-full h-15 px-2 items-center bg-red-500 flex justify-between'>
@@ -53,10 +40,8 @@ return (
         </div>
 
         <div className='w-full h-20 flex justify-center items-center'>
-          <button onClick={() => {
-            cleared()
-            placeOrder()       }} 
-            className={`w-30 h-10 bg-black text-white rounded ${items.length == 0 ? 'hidden' : 'block opacity-90'}`}>Place order
+          <button onClick={() => { placeOrder()  }} 
+            className={`w-30 h-10 bg-black text-white cursor-pointer rounded ${items.length == 0 ? 'hidden' : 'block opacity-90'} hover:bg-blue-600 active:scale-90`}>Place order
           </button>
         </div>
      </div>
