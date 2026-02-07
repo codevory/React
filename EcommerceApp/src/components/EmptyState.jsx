7b602bcfe28d294f8be34c1d3b369d1858a70185
codevory <shahijahan.codes@gmail.com>
2026-02-07 15:34:18 +0530

    Created  components

---
+++ EcommerceApp/src/components/EmptyState.jsx
@@ -0,0 +1,13 @@
+const EmptyState = () => {
+  return (
+   <div className="text-center mt-20">
+  <h2 className="text-xl font-semibold">No results</h2>
+  <p className="text-gray-500 mt-2">
+    Try searching for phones, chargers, accessories…
+  </p>
+</div>
+
+  )
+}
+
+export default EmptyState
---
+++ EcommerceApp/src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+  return (
+   <div className='absolute top-1/2 left-1/2 w-10 h-10  
+   transition-transform rounded-full border-3 border-l-red-500 border-t-green-500
+    border-r-blue-700 animate-spin '></div>
+  )
+}
+
+export default Spinner
