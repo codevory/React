96c87b5ad77a73fb8c74d8d41a3b52947df2645d
codevory <shahijahan.codes@gmail.com>
2026-02-07 16:32:50 +0530

    fixed total value overflowing

--- EcommerceApp/src/store/cartSelectors.js
+++ EcommerceApp/src/store/cartSelectors.js
@@ -3,7 +3,7 @@ import { createSelector } from "@reduxjs/toolkit";
 export const selectCartItems = (state) => state.cart.items;
 
 export const selectCartTotal = createSelector([selectCartItems], (items) =>
-  items.reduce((acc, item) => acc + item.price * item.qty, 0),
+  items.reduce((acc, item) => acc + item.price * item.qty, 0).toFixed(2),
 );
 
 export const selectCartCount = createSelector([selectCartItems], (items) =>
