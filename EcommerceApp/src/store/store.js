af4895946f6d2f16929b67af3665b2db1607a99b
codevory <shahijahan.codes@gmail.com>
2026-02-07 15:44:05 +0530

    added middleWare in redux store to handle side effects

--- EcommerceApp/src/store/store.js
+++ EcommerceApp/src/store/store.js
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import searchSlice from "./searchSlice";
 import cartSlice from "./cartSlice";
+import { cartMiddleWare } from "./cartMiddleware";
+import { cartPersistMiddleware } from "./cartPersistMiddleware";
 const store = configureStore({
   reducer: {
     search: searchSlice,
     cart: cartSlice,
   },
+  middleware: (getDefaultMiddleWare) =>
+    getDefaultMiddleWare().concat(cartMiddleWare).concat(cartPersistMiddleware),
 });
 
 export default store;
