567705b7aa1e852f91cb201ca15eb5c167d4769b
codevory <shahijahan.codes@gmail.com>
2026-03-17 16:11:47 +0530

    app.tsx file

--- Typescript/src/App.tsx
+++ Typescript/src/App.tsx
@@ -1,16 +1,21 @@
 import './App.css'
-import ProcessOrderForm from './components/ProcessOrderForm'
+import { useState } from 'react'
+import Counter from './provider/counterContext'
+import TodoApp from './components/Todo';
 
 function App() {
-
+const [value,setValue] = useState<number>(0);
+function increment(){
+setValue((p) => p + 1);
+}
+function decrement(){
+  setValue((p) => p - 1)
+}
   return (
     <>
       <div>
-       <h2>App</h2>
-       <ProcessOrderForm onSubmit={(order) => {
-        console.log("order placed",order);
-       }
-       } />
+       <Counter value={value} increment={increment} decrement={decrement} />
+       <TodoApp />
       </div>
     </>
   )
