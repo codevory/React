4662495fc8db840069ed4623c423b242b44cd70a
codevory <shahijahan.codes@gmail.com>
2026-03-18 18:32:25 +0530

    data presenter component

---
+++ Typescript/src/components/DataPresenter.tsx
@@ -0,0 +1,46 @@
+//import React,{ useState } from 'react'
+import '../App.css'
+
+interface DataPresenterProps {
+id:number;
+title:string;
+price:number;
+rating:number;
+description:string;
+category:string;
+tags:string[];
+image:string;
+dataLen:number;
+}
+
+const DataPresenter = ({dataLen,id,title,price,rating,description,category,tags,image}:DataPresenterProps) => {
+
+  if(dataLen === 0) return <div className='page'>
+    <h2>No data found </h2>
+    <p>Search above eg. phone,laptops etc</p>
+   </div>
+
+  return (
+    <div className="dataOuter">
+      <img src={image} alt={title} />
+
+      <div className="divCol">
+           <div className="divFlex">
+             <h2>{title}</h2> 
+             <span>{id}</span>
+              <p>{rating}</p>
+            </div>
+         <p>{description}</p>
+      </div>
+
+      <div className="divFlex">
+      <p>{category}</p>
+      <p>{price}</p>
+      </div>
+      
+      <span>{tags}</span>
+    </div>
+  )
+}
+
+export default DataPresenter
