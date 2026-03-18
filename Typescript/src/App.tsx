0ad19beabd708e0c276754e858d8c9bae5552a56
codevory <shahijahan.codes@gmail.com>
2026-03-18 14:42:01 +0530

    code refactoring

--- Typescript/src/App.tsx
+++ Typescript/src/App.tsx
@@ -1,32 +1,12 @@
 import './App.css'
-import { useState } from 'react'
-import Counter from './provider/counterContext'
-import TodoApp from './components/Todo';
-import TaskTracker from './components/TaskTracker';
+import TaskTrackerParent from './components/TaskTrackerParent';
 
 function App() {
-const [value,setValue] = useState<number>(0);
-const [done,setIsDone] = useState<boolean>(false)
 
-function toggle(id:number){
-if(id === id){
-  setIsDone((p) => !p)
-}
-}
-
-
-function increment(){
-setValue((p) => p + 1);
-}
-function decrement(){
-  setValue((p) => p - 1)
-}
   return (
     <>
       <div>
-       <Counter value={value} increment={increment} decrement={decrement} />
-       <TodoApp />
-       <TaskTracker handleToggle={toggle} id={1} label='Hey i need to finish task Tracker component' isCompleted={done}  />
+       <TaskTrackerParent />
       </div>
     </>
   )
