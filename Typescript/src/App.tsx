25e7159c6211a825d865948678ff75d05fc75233
codevory <shahijahan.codes@gmail.com>
2026-03-18 13:08:42 +0530

    added task Tracker component

--- Typescript/src/App.tsx
+++ Typescript/src/App.tsx
@@ -2,9 +2,19 @@ import './App.css'
 import { useState } from 'react'
 import Counter from './provider/counterContext'
 import TodoApp from './components/Todo';
+import TaskTracker from './components/TaskTracker';
 
 function App() {
 const [value,setValue] = useState<number>(0);
+const [done,setIsDone] = useState<boolean>(false)
+
+function toggle(id:number){
+if(id === id){
+  setIsDone((p) => !p)
+}
+}
+
+
 function increment(){
 setValue((p) => p + 1);
 }
@@ -16,9 +26,11 @@ function decrement(){
       <div>
        <Counter value={value} increment={increment} decrement={decrement} />
        <TodoApp />
+       <TaskTracker handleToggle={toggle} id={1} label='Hey i need to finish task Tracker component' isCompleted={done}  />
       </div>
     </>
   )
 }
+
 
 export default App
